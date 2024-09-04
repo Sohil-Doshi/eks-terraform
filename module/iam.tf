6a96bee5f0217ef90c6b077a70055495861ed03e
Sohil Doshi <93750240+sohil344@users.noreply.github.com>
2024-09-04 18:26:10 +0530

    Update iam.tf added comments

--- module/iam.tf
+++ module/iam.tf
@@ -7,10 +7,11 @@ resource "random_integer" "random_suffix" {
   max = 9999
 }
 
+#This IAM role is critical for the EKS control plane, allowing it to interact with other AWS services (like creating and managing resources).
 resource "aws_iam_role" "eks-cluster-role" {
-  count = var.is_eks_role_enabled ? 1 : 0
+  count = var.is_eks_role_enabled ? 1 : 0  #Uses a conditional expression to create the role only if is_eks_role_enabled is set to true. If it's false, the role is not created.
   name  = "${local.cluster_name}-role-${random_integer.random_suffix.result}"
-
+#Defines a trust policy allowing EKS to assume the role
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -23,6 +24,7 @@ resource "aws_iam_role" "eks-cluster-role" {
   })
 }
 
+#This policy allows the EKS cluster to interact with AWS resources, such as EC2, EBS, etc.
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   count      = var.is_eks_role_enabled ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -51,6 +53,7 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonWorkerNodePolicy" {
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
 
+#This policy grants permissions required for pod networking, including interacting with VPC resources and managing ENIs (Elastic Network Interfaces).
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
   count      = var.is_eks_nodegroup_role_enabled ? 1 : 0
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
@@ -68,7 +71,7 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
   role       = aws_iam_role.eks-nodegroup-role[count.index].name
 }
 
-# OIDC
+# OIDC-This role allows EKS pods to use IAM roles via OIDC, enabling fine-grained permissions for specific workloads without exposing long-lived credentials.
 resource "aws_iam_role" "eks_oidc" {
   assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
   name               = "eks-oidc"
