b2c70aa813dd743e6dccceaa42948a3b532eb965
Sohil Doshi <93750240+sohil344@users.noreply.github.com>
2024-09-04 16:30:57 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "terraform-state-file-dt"
+    bucket = "terraform-state-file-six-tier-fleet-management-app"
     key    = "test/aws_infra"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
